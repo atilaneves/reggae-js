4945aeaac4c95f40b062fae28dbab38da0c0ef37
Atila Neves <atila.neves@gmail.com>
2015-09-11 18:49:27 +0200

    Passing leaf UT

--- reggae.js
+++ reggae.js
@@ -1,6 +1,12 @@
 exports.Target = function (output) {
+
     this.outputs = [output]
+    this.type = "fixed"
+    this.command = {}
+    this.dependencies = {type: "fixed", targets: []}
+    this.implicits = {type: "fixed", targets: []}
+
     this.toJson = function () {
-        return '{}'
+        return JSON.stringify(this)
     }
 }
--- test.js
+++ test.js
@@ -1,15 +1,27 @@
 var reggae = require('./reggae.js')
 
-exports.testLeaf = function(test) {
-    //var tgt = new Target('foo.d')
+function formatJson(json) {
+    return JSON.stringify(JSON.parse(json))
+}
+
+exports.testLeafFoo = function(test) {
     var tgt = new reggae.Target('foo.d')
-    test.equal(tgt.toJson(),
-               '{"type": "fixed",\
-                 "command": {},\
-                 "outputs": ["foo.d"],\
-                 "dependencies": {"type": "fixed", "targets": []},\
-                 "implicits": {"type": "fixed", "targets": []}}',
-               "value is '" + tgt.toJson() + "'")
-    test.strictEqual("bar", "bar")
+    test.deepEqual(JSON.parse(tgt.toJson()),
+                   {type: "fixed",
+                    command: {},
+                    outputs: ["foo.d"],
+                    dependencies: {type: "fixed", targets: []},
+                    implicits: {type: "fixed", targets: []}})
+    test.done()
+}
+
+exports.testLeafBar = function(test) {
+    var tgt = new reggae.Target('bar.d')
+    test.deepEqual(JSON.parse(tgt.toJson()),
+                   {type: "fixed",
+                    command: {},
+                    outputs: ["bar.d"],
+                    dependencies: {type: "fixed", targets: []},
+                    implicits: {type: "fixed", targets: []}})
     test.done()
 }
